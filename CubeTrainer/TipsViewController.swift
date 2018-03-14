547f913766c2bfd1717b5dbd8d155b6149c7ce86
Phurinat Puekkham <phurinaix@gmail.com>
2018-03-14 23:54:27 +0700

    Update TipsViewController.swift

--- CubeTrainer/TipsViewController.swift
+++ CubeTrainer/TipsViewController.swift
@@ -1,35 +1,12 @@
-//
-//  TipsViewController.swift
-//  CubeTrainer
-//
-//  Created by user on 20/12/2560 BE.
-//  Copyright © 2560 ANT. All rights reserved.
-//
-
 import UIKit
 
 class TipsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
