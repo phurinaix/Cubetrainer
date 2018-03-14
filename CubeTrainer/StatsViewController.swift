5202e37847f97daf85861f715cf24eb2e1b614eb
Phurinat Puekkham <phurinaix@gmail.com>
2018-03-14 23:56:26 +0700

    Update StatsViewController.swift

--- CubeTrainer/StatsViewController.swift
+++ CubeTrainer/StatsViewController.swift
@@ -1,11 +1,3 @@
-//
-//  StatsViewController.swift
-//  CubeTrainer
-//
-//  Created by user on 19/12/2560 BE.
-//  Copyright © 2560 ANT. All rights reserved.
-//
-
 import UIKit
 
 class StatsViewController: UIViewController {
@@ -29,17 +21,5 @@ class StatsViewController: UIViewController {
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
