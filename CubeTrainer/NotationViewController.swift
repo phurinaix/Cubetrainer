d29d06c62e29668ba8932a6ef98f919c8c56538d
Phurinat Puekkham <phurinaix@gmail.com>
2018-03-24 23:08:30 +0700

    Update NotationViewController.swift

--- CubeTrainer/NotationViewController.swift
+++ CubeTrainer/NotationViewController.swift
@@ -1,3 +1,11 @@
+//
+//  ViewController.swift
+//  CubeTrainer
+//
+//  Created by user on 19/12/2560 BE.
+//  Copyright © 2560 ANT. All rights reserved.
+//
+
 import UIKit
 
 class NotationViewController: UIViewController {
