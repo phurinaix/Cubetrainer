8643a973c58bff76fb471034114301867696373a
Phurinat Puekkham <phurinaix@gmail.com>
2018-03-24 23:08:15 +0700

    Update AboutViewController.swift

--- CubeTrainer/AboutViewController.swift
+++ CubeTrainer/AboutViewController.swift
@@ -1,4 +1,12 @@
 //About Developer
+//
+//  ViewController.swift
+//  CubeTrainer
+//
+//  Created by user on 19/12/2560 BE.
+//  Copyright © 2560 ANT. All rights reserved.
+//
+
 import UIKit
 
 class AboutViewController: UIViewController {
