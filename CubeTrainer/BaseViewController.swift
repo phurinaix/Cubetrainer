786a6dc743c7774c3b9ca2bda609f2d0447eac21
Phurinat Puekkham <phurinaix@gmail.com>
2018-04-08 23:51:50 +0700

    Update BaseViewController.swift

--- CubeTrainer/BaseViewController.swift
+++ CubeTrainer/BaseViewController.swift
@@ -5,7 +5,6 @@ class BaseViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
