bd26d40ea8804a691dd5c442f304c8dc0912b339
Phurinat Puekkham <phurinaix@gmail.com>
2018-04-20 23:32:49 +0700

    Update TipsViewController.swift

--- CubeTrainer/TipsViewController.swift
+++ CubeTrainer/TipsViewController.swift
@@ -5,7 +5,6 @@ class TipsViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
