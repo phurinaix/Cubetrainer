ad83f06b48f6b1af41fe3efd53c5f0b81c5b2a45
Phurinat Puekkham <phurinaix@gmail.com>
2018-06-09 22:56:23 +0700

    Update TermsViewController.swift

--- CubeTrainer/TermsViewController.swift
+++ CubeTrainer/TermsViewController.swift
@@ -1,11 +1,9 @@
 import UIKit
 
 class TermsViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
